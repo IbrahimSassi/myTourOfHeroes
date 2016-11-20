7fa3fa3d8cd0f55482564d9ed3814bad0c2d2c60
IbrahimSassi <ibrahim.sassi@esprit.tn>
2016-11-20 20:03:10 +0100

    Search-by-name

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule }    from '@angular/forms';
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent}  from './hero-search.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 import { AppRoutingModule }     from './app-routing.module';
 import {HttpModule} from "@angular/http";
 import {InMemoryDbService, InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
-
+import './rxjs-extensions';
 @NgModule({
   imports: [
     BrowserModule,
@@ -23,7 +24,8 @@ import {InMemoryDataService} from "./in-memory-data.service";
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
